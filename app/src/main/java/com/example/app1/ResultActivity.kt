7dc1465859a45e6dd160ee3f40d948f3dc379fb9
Nate Lanza <dolphinpower900@gmail.com>
2023-02-15 17:20:37 -0700

    Displaying first and last name!!!

--- app/src/main/java/com/example/app1/ResultActivity.kt
+++ app/src/main/java/com/example/app1/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.app1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.app1.databinding.ActivityFormBinding
 import com.example.app1.databinding.ActivityResultBinding
 
@@ -16,16 +17,16 @@ class ResultActivity : AppCompatActivity() {
 
         // Get intent
         val intent = intent
-        val firstName = intent.getStringExtra("firstName")
-        val lastName = intent.getStringExtra("lastName")
-        println("firstName: $firstName")
-        println("lastName: $lastName")
 
+        // Assign text fields based on input
+        val firstNameField = findViewById<TextView>(R.id.firstNameText)
+        val lastNameField = findViewById<TextView>(R.id.lastNameText)
         if (savedInstanceState != null) {
-            binding.firstNameText.text = savedInstanceState.getString("firstName")
-            binding.lastNameText.text = savedInstanceState.getString("lastName")
+            firstNameField.text = savedInstanceState.getString("firstName")
+            lastNameField.text = savedInstanceState.getString("lastName")
         } else {
-
+            firstNameField.text = intent.getStringExtra("firstName")
+            lastNameField.text = intent.getStringExtra("lastName")
         }
     }
 }
